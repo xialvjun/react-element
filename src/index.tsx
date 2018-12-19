775fb144a8c62179d6360165a52bcc5b15e4f199
xialvjun <xialvjun@live.com>
2018-12-19 18:42:13 +0800

    init_api

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,7 @@ import immutagen from "immutagen";
 export interface ElementProps {
   construct?(ele, props: ElementProps): void;
   componentDidMount?(ele: Element): void;
-  shouldComponentUpdate?(
-    ele: Element,
-    nextProps: ElementProps,
-    nextState
-  ): boolean;
+  shouldComponentUpdate?(ele: Element, nextProps: ElementProps, nextState): boolean;
   componentDidUpdate?(ele: Element, prevProps: ElementProps, prevState): void;
   componentWillUnmount?(ele: Element): void;
   chidlren?: ((ele: Element) => ReactNode) | ReactNode;
@@ -27,9 +23,7 @@ export class Element extends Component<ElementProps> {
   }
   shouldComponentUpdate(nextProps, nextState) {
     const { shouldComponentUpdate } = this.props;
-    return shouldComponentUpdate
-      ? shouldComponentUpdate(this, nextProps, nextState)
-      : true;
+    return shouldComponentUpdate ? shouldComponentUpdate(this, nextProps, nextState) : true;
   }
   componentDidUpdate(prevProps, prevState) {
     const { componentDidUpdate } = this.props;
@@ -51,20 +45,18 @@ export const init_value = value => ele => {
     ele.value = value;
     ele.forceUpdate(callback);
   };
-  ele.set_partial = (path, path_value) =>
-    ele.set_value(set_path(ele.value, path, path_value));
+  ele.set_partial = (path, path_value) => ele.set_value(set_path(ele.value, path, path_value));
 };
 
 export const init_state = state => ele => {
   ele.state = state;
   ele.set_state = (...args) => ele.setState(...args);
-  ele.set_partial = (path, path_value) =>
-    ele.set_state(set_path(ele.state, path, path_value));
+  ele.set_partial = (path, path_value) => ele.set_state(set_path(ele.state, path, path_value));
 };
 
 export const init_ref = (name: string) => ele => {
   name = name || "ref";
-  ele[name] = (React as any).createRef();
+  ele[name] = React.createRef();
 };
 
 export const init_refs = (name: string) => ele => {
@@ -73,12 +65,33 @@ export const init_refs = (name: string) => ele => {
     {
       get(obj, prop) {
         if (!obj[prop]) {
-          obj[prop] = (React as any).createRef();
+          obj[prop] = React.createRef();
         }
         return obj[prop];
       }
     }
   );
+};
+
+export const init_api = (api: (...variables: any[]) => Promise<any>, variables?: any[]) => ele => {
+  const loading = Object.prototype.toString.apply(variables) === "[object Array]";
+  init_state({ data: null, error: null, loading })(ele);
+  ele.api = async (...args) => {
+    if (!ele.state.loading) {
+      ele.set_state({ loading: true });
+    }
+    try {
+      const data = await api(...args);
+      ele.set_state({ data, error: null, loading: false });
+      return data;
+    } catch (error) {
+      ele.set_state({ error, loading: false });
+      throw error;
+    }
+  };
+  if (loading) {
+    ele.api(...variables);
+  }
 };
 
 const compose = ({ next, value }) => {
